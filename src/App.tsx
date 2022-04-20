64fa29c287dd865bfe9cb925519e82f3d9080460
Andres Pacheco <apacheco73@hotmail.com>
2022-04-20 08:43:29 -0300

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
         <p>Welcome to Football Manager</p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
