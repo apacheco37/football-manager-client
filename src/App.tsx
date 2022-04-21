32ecf7a8004368d3afb26ef7b5597d19c9609410
Andres Pacheco <apacheco73@hotmail.com>
2022-04-20 21:00:26 -0300

    Add emotion package for css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import "./App.css";
+import styled from "@emotion/styled";
+
+const AppDiv = styled.div({
+  textAlign: "center",
+});
+
+const Header = styled.header({
+  backgroundColor: "#282c34",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppDiv>
+      <Header>
         <p>Welcome to Football Manager</p>
-      </header>
-    </div>
+      </Header>
+    </AppDiv>
   );
 }
 
