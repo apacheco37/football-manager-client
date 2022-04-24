62e0a9884b5f0fa758a6a6564fb47195e6e09711
Andres Pacheco <apacheco73@hotmail.com>
2022-04-24 20:06:59 -0300

    Add Apollo Client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
 import "./index.css";
 import App from "./App";
 
+const link = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+  credentials: "include",
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
